0486064cd7468fa53bfafd2e30bae7d6f5a203ec
yogyasinghal <48852737+yogyasinghal@users.noreply.github.com>
2020-09-08 01:05:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.post('/home',function(req,res){
     res.render('home.ejs',{ticket:ticket,ticket_copy:JSON.stringify(ticket) , number:parseInt(number),name:name,board:board,arr: JSON.stringify(arr)})
 });
 
-
-app.listen(3000);
-console.log("You are at port 3000"); 
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT);
 
 
